87c07c6cc680699cbec61a6b3929d95da0847d21
Cameron Drake <cdrake757@gmail.com>
2012-06-26 10:38:12 -0700

    Updated MC Hammer cover
    This version of the cover features the Ken Burns photo effect. There is
    still much tweaking to do on this effect and also there is a known
    issue with the menu that Ken and I need to resolve.

--- tim-oreilly/js/main.js
+++ tim-oreilly/js/main.js
@@ -2,13 +2,20 @@ window.onload = init;
 
 function init() {
 	var nav = document.querySelector('#menu-button');
-	//console.log(nav);
+	//console.log('click', nav);
 	nav.onclick = navClick	
 }
 
 function navClick(e) {
+	//console.log('CLICK!')
 	var cover = document.querySelector('#cover');
+	//console.log(cover)
 	cover.className= (cover.className=="active")? "":"active";
 	var nav = document.querySelector('#nav');
 	nav.className= (nav.className=="active")? "":"active";
+
+	var menu = document.querySelector('#menu-button');
+	menu.className= (menu.className=="active")? "":"active";
 }
+
+
